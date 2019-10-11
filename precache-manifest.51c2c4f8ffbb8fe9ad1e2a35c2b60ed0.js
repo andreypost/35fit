0c5a4580c115c5808164972090261a2b5831dded
Andrey Postolyuk <postoliuk.av@gmail.com>
2019-10-11 15:49:42 +0300

    Updates

---
+++ precache-manifest.51c2c4f8ffbb8fe9ad1e2a35c2b60ed0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fdd6b4d91a05ee9a31d04c848ff62f",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "c53b80019f3450b4e029",
+    "url": "/my-react-app/static/css/main.0f1c825d.chunk.css"
+  },
+  {
+    "revision": "245926d3e1505723080f",
+    "url": "/my-react-app/static/js/2.6f805d02.chunk.js"
+  },
+  {
+    "revision": "c53b80019f3450b4e029",
+    "url": "/my-react-app/static/js/main.acee839b.chunk.js"
+  },
+  {
+    "revision": "07add1d6cd3da92baf78",
+    "url": "/my-react-app/static/js/runtime-main.41c2fd6d.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/my-react-app/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/my-react-app/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/my-react-app/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/my-react-app/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "01360a5f85636a45402b3bcbac679f0a",
+    "url": "/my-react-app/static/media/add_1_1.01360a5f.png"
+  },
+  {
+    "revision": "f442cf821431eedea6676e2611efa004",
+    "url": "/my-react-app/static/media/add_1_2.f442cf82.png"
+  },
+  {
+    "revision": "fe2dd8ef5743167b5a64a891d4c51391",
+    "url": "/my-react-app/static/media/add_1_3.fe2dd8ef.png"
+  },
+  {
+    "revision": "91a7aace9d4fc1820772902cc1754cbe",
+    "url": "/my-react-app/static/media/add_2_0.91a7aace.png"
+  },
+  {
+    "revision": "5c5199109f79066597dad9e3fbf6131b",
+    "url": "/my-react-app/static/media/banner_bg_1.5c519910.png"
+  },
+  {
+    "revision": "1785575b5859838656cd8a6c1796a572",
+    "url": "/my-react-app/static/media/banner_bg_3.1785575b.png"
+  },
+  {
+    "revision": "770364b60fa4214f8fb798304380fe9e",
+    "url": "/my-react-app/static/media/banner_bg_4.770364b6.png"
+  },
+  {
+    "revision": "e96ad13ed6f963266d248ec81bc585ca",
+    "url": "/my-react-app/static/media/banner_bg_5.e96ad13e.png"
+  },
+  {
+    "revision": "e82a445e9cd7522a79f2f6a123e6d5cd",
+    "url": "/my-react-app/static/media/banner_bg_6.e82a445e.png"
+  },
+  {
+    "revision": "a05f459494028482959c620e5c922101",
+    "url": "/my-react-app/static/media/banner_bg_7.a05f4594.png"
+  },
+  {
+    "revision": "39eb3c16628158945023076022910aaa",
+    "url": "/my-react-app/static/media/comm_1.39eb3c16.png"
+  },
+  {
+    "revision": "4a113fe3fd335bc6cc63d584ca82c144",
+    "url": "/my-react-app/static/media/comm_2.4a113fe3.png"
+  },
+  {
+    "revision": "f91f2d0f8549e6b3e151a65cc57106e6",
+    "url": "/my-react-app/static/media/comm_3.f91f2d0f.png"
+  },
+  {
+    "revision": "4a353f3309403d544ca52a9c1370f867",
+    "url": "/my-react-app/static/media/icon_2_15.4a353f33.png"
+  },
+  {
+    "revision": "503de59e9202139e1cce99193c65d293",
+    "url": "/my-react-app/static/media/icon_2_16.503de59e.png"
+  },
+  {
+    "revision": "2907e722314ce6c68cf40362dd0ddb97",
+    "url": "/my-react-app/static/media/pattern_bg_10_1.2907e722.png"
+  },
+  {
+    "revision": "e204899f6aa016286a547a287db51650",
+    "url": "/my-react-app/static/media/pattern_bg_1_0.e204899f.png"
+  },
+  {
+    "revision": "3bc812b61785179249fc7ce1b6164a34",
+    "url": "/my-react-app/static/media/pattern_bg_1_1.3bc812b6.png"
+  },
+  {
+    "revision": "e4b7413b5864dc8e94034328a0de1f0e",
+    "url": "/my-react-app/static/media/pattern_bg_1_1_1.e4b7413b.png"
+  },
+  {
+    "revision": "ff07aa08c86c467b5868bf8b8d1a87c6",
+    "url": "/my-react-app/static/media/pattern_bg_1_2.ff07aa08.png"
+  },
+  {
+    "revision": "dc541ecc7c556b39b19a2e998e2c364f",
+    "url": "/my-react-app/static/media/pattern_bg_1_4.dc541ecc.png"
+  },
+  {
+    "revision": "ab5363b47760cb6a4e485b3c3ca43f52",
+    "url": "/my-react-app/static/media/pattern_bg_1_4_1.ab5363b4.png"
+  },
+  {
+    "revision": "93279838b6a4adcdc08914f7de8e9015",
+    "url": "/my-react-app/static/media/pattern_bg_1_5.93279838.png"
+  },
+  {
+    "revision": "4eaa7be59b927a1cb4a4971c6e68d07c",
+    "url": "/my-react-app/static/media/pattern_bg_2_3.4eaa7be5.png"
+  },
+  {
+    "revision": "93bda62969f12ed3b38ddcaa1cdf457d",
+    "url": "/my-react-app/static/media/pattern_bg_3_1.93bda629.png"
+  },
+  {
+    "revision": "38920e90606155b3b03c766b734e8940",
+    "url": "/my-react-app/static/media/pattern_bg_4_1.38920e90.png"
+  }
+]);
